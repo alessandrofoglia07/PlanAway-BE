1a3c57d129a790a8c603e9ffe1b351065352e2c0
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2023-03-17 22:32:38 +0100

    added cors support

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,12 @@
 import express from "express";
+import cors from 'cors';
 // npm start to compile and run
 const app = express();
 const port = 3002;
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.listen(port, () => console.log(`App running on port ${port}`));
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import express, {Application, Response, Request} from "express";
+import cors from 'cors';
 
 // npm start to compile and run
 const app : Application = express();
 const port = 3002;
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.listen(port, ()=>console.log(`App running on port ${port}`));
