fb401a809f3a3c61cc35c788c0dfd8e7eff8f140
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2023-03-25 15:10:30 +0100

    added MySQL db and signup functionality, bcrypt

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from 'cors';
+import mysql from 'mysql';
+import bcrypt from 'bcrypt';
 // npm start to compile and run
 const app = express();
 const port = 3002;
@@ -10,3 +12,57 @@ app.use(cors({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.listen(port, () => console.log(`App running on port ${port}`));
+// bcrypt 
+const SaltRounds = 10;
+// db connection
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: 'root123',
+    database: 'planaway'
+});
+db.connect((err) => {
+    if (err) {
+        console.log('Error connecting' + err.stack);
+    }
+    else {
+        console.log('Connected to MySQL db');
+    }
+});
+app.post('/signup', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
+    db.query(`SELECT * FROM users WHERE email = '${email}'`, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500);
+        }
+        else {
+            if (result.length > 0) {
+                res.send({ message: 'Email is already registered' });
+                console.log('Email is already registered');
+            }
+            else {
+                bcrypt.hash(password, SaltRounds, (err, hash) => {
+                    if (err) {
+                        res.status(500);
+                        console.log(err);
+                    }
+                    else {
+                        db.query(`INSERT INTO users (username, password, email) VALUES ('${username}', '${hash}', '${email}')`, (err, result) => {
+                            if (err) {
+                                res.status(500);
+                                console.log(err);
+                            }
+                            else {
+                                res.status(201).send({ message: 'User created' });
+                                console.log('Sign up successful');
+                            }
+                        });
+                    }
+                });
+            }
+        }
+    });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express, {Application, Response, Request} from "express";
 import cors from 'cors';
+import mysql, { MysqlError } from 'mysql';
+import bcrypt, { hash } from 'bcrypt';
 
 // npm start to compile and run
 const app : Application = express();
@@ -12,3 +14,58 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.listen(port, ()=>console.log(`App running on port ${port}`));
+
+// bcrypt 
+const SaltRounds = 10;
+
+// db connection
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: 'root123',
+    database: 'planaway'
+})
+
+db.connect((err) => {
+    if (err) {
+        console.log('Error connecting' + err.stack);
+    } else {
+        console.log('Connected to MySQL db');
+    }
+})
+
+
+app.post('/signup', (req: Request, res: Response) => {
+    const username : string = req.body.username;
+    const password : string = req.body.password;
+    const email : string = req.body.email;
+
+    db.query(`SELECT * FROM users WHERE email = '${email}'`, (err: MysqlError, result: string) => {
+        if (err) {
+            console.log(err);
+            res.status(500);
+        } else {
+            if (result.length > 0) {
+                res.send({message: 'Email is already registered'});
+                console.log('Email is already registered');
+            } else {
+                bcrypt.hash(password, SaltRounds, (err : Error | undefined, hash : string) => {
+                    if (err) {
+                        res.status(500)
+                        console.log(err);
+                    } else {
+                        db.query(`INSERT INTO users (username, password, email) VALUES ('${username}', '${hash}', '${email}')`, (err : MysqlError, result : string) => {
+                            if (err) {
+                                res.status(500)
+                                console.log(err);
+                            } else {
+                                res.status(201).send({message: 'User created'});
+                                console.log('Sign up successful');
+                            }
+                        })
+                    }
+                })
+            }
+        }
+    })
+})
